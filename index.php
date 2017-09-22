47166257bd0650f3ba14afbfd6f871522931fbd8
Gun <gunnn_boy_hand@hotmail.com>
2017-09-22 12:01:23 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="reset.css">
+  </head>
+  <body>
+    <?php include 'navbar.php'; ?>
+    <center>
+    <div style="background-color: #eeeeee;">
+      <div id="carouselExampleSlidesOnly" style="width: 50%;" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100" src="assets/img/slide1.png" alt="First slide" >
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100" src="assets/img/slide4.png" alt="Third slide" >
+          </div>
+        </div>
+      </div>
+    </div>
+    <div style="width: 90%; margin-left: 5%; border-left:1px solid #fafafa; border-right:1px solid #fafafa; float: left; display: inline-block;">
+      <div style="width: 80%; float: left; display: inline-block; margin-left: 10%;">
+        <div class="card" style=" width: 15rem; float: left;">
+          <img class="card-img-top" src="assets/img/mmis.png" style="width: 120px; padding-top: 10px;" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">MMis</h4>
+            <p class="card-text" style="font-size: 14pt;">รัฐมนตรีว่าการกระทรวงสาธารณสุข นายปิยะสกล สกลสัตยาทร. <br>ปลัดกระทรวงสาธารณสุข. <br>นายโสภณ เมฆธน. รองปลัดกระทรวงสาธารณสุข นายสมศักดิ์ อรรฆศิลป์ นางประนอม คำเที่ยง...</p><br>
+            <a href="#" class="btn">รายละเอียด...</a>
+          </div>
+        </div>
+        <div class="card" style=" width: 15rem; float: left;">
+          <img class="card-img-top" src="assets/img/mmis.png" style="width: 120px; padding-top: 10px;" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">Mmis</h4>
+            <p class="card-text" style="font-size: 14pt;">รับสมัครสอบแข่งขันเพื่อบรรจุและแต่งตั้งบุคคลเข้ารับราชการ ในตำแหน่งนักวิชาการคอมพิวเตอร์ปฏิบัติการ นักวิชาการสาธารณสุขปฏิบัติการ เจ้าพนักงานสาธารณสุขปฏิบัติงาน ...</p><br>
+            <a href="#" class="btn">รายละเอียด...</a>
+          </div>
+        </div>
+        <div class="card" style=" width: 15rem; float: left;">
+          <img class="card-img-top" src="assets/img/mmis.png" style="width: 120px; padding-top: 10px;" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">mmIs</h4>
+            <p class="card-text" style="font-size: 14pt;">นื่องด้วย สำนักบริหารการสาธารณสุข ได้นำข้อมูลพื้นฐานโรงพยาบาลสังกัดสำนักงานปลัดกระทรวงสาธารณสุข ได้แก่ จำนวนเตียงจริง จำนวนเตียง ICU จำนวนห้องผ่าตัด ข้อมูล CMI ...</p><br>
+            <a href="#" class="btn">รายละเอียด...</a>
+          </div>
+        </div>
+        <div class="card" style=" width: 15rem; float: left;">
+          <img class="card-img-top" src="assets/img/mmis.png" style="width: 120px; padding-top: 10px;" alt="Card image cap">
+          <div class="card-body">
+            <h4 class="card-title">mmiS</h4>
+            <p class="card-text" style="font-size: 14pt;">ปิยะสกล สกลสัตยาทร รัฐมนตรีว่าการกระทรวงสาธารณสุข ร่วมกับ สมชาย เสียงหลาย ที่ปรึกษารัฐมนตรีว่าการกระทรวง ยุติธรรมเป็นประธานในพิธีเปิดงาน "คืนคน สุขภาพดีสู่สังคม" ...</p><br>
+            <a href="#" class="btn">รายละเอียด...</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    </center>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  </body>
+  <style type="text/css">
+    .card:hover{
+      background-color: #eeeeee;
+      transition:color .2s ease-out, background 0.1s ease-in;
+    }
+  </style>
+</html>
---
+++ navbar.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <img src="assets/img/1.jpg" style="width: 60px;">&nbsp
+  <img src="assets/img/2.png" style="width: 60px;">&nbsp
+  <img src="assets/img/3.png" style="width: 60px;">&nbsp
+  <img src="assets/img/4.png" style="width: 60px;">
+  <img src="assets/img/5.png" style="width: 60px;">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav" style=" margin-left: 64%; font-weight: bold; font-size: 15pt;">
+      <li class="nav-item">
+        <a class="nav-link" href="#">เกี่ยวกับคลังยา <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          บริการ
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">mmis</a>
+          <a class="dropdown-item" href="#">mmpcd</a>
+          <a class="dropdown-item" href="#">อื่นๆ</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">สำหรับเจ้าหน้าที่</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ติดต่อ</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div style="border-top: 1px solid gray; opacity: 0.4;"></div>
